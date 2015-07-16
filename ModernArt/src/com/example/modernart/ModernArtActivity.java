2c14c0f2f18a6d81f170938cb253dd65939a1c9c
Kostiantyn Makohoniuk <specdiiin@gmail.com>
2015-07-16 16:56:14 +0300

    update format of code

--- ModernArt/src/com/example/modernart/ModernArtActivity.java
+++ ModernArt/src/com/example/modernart/ModernArtActivity.java
@@ -75,7 +75,6 @@ public class ModernArtActivity extends Activity {
 
 	    @Override
 		public boolean onCreateOptionsMenu(Menu menu) {
-			// Inflate the menu; this adds items to the action bar if it is present.
 			getMenuInflater().inflate(R.menu.modern_art, menu);
 			return true;
 		}
@@ -84,9 +83,6 @@ public class ModernArtActivity extends Activity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_more_information) {
             DialogFragment moreInfoFragment = new MoreInfoDialog();
